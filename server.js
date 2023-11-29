9cf404e8176fff1b831a1b19177e6c93e77fb473
Mampuru <mampuruntwampe@gmail.com>
2023-11-29 02:22:32 +0200

    Updated configure session middleware

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express'); 
+const express = require('express');
+const session = require('express-session');
 
 const app = express(); 
 const PORT = process.env.PORT || 3001;
