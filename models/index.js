d90cb20397af6f4e79b52a183360a6bc6fb6fb9c
Mampuru <mampuruntwampe@gmail.com>
2023-11-29 12:52:55 +0200

    Addition of table relationships

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Issue = require('./Issue');
+const Comment = require('./Comment');
+
+Issue.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Issue
+};
