7348be4a757a85345cac964b4d98f7d24b59d5b8
Mampuru <mampuruntwampe@gmail.com>
2023-12-01 02:49:50 +0200

    Fixed runtime errors on the server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express'); 
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 
 const app = express(); 
 const PORT = process.env.PORT || 3001;
