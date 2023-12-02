a7423045a635303a50dcfb9990ca6922cd0dcbe1
Mampuru <mampuruntwampe@gmail.com>
2023-12-02 12:30:39 +0200

    Updated the auth middleware to handle JWT

--- utils/auth.js
+++ utils/auth.js
@@ -10,5 +10,22 @@ const withAuth = (req, res, next) => {
     }
   };
   
-  module.exports = withAuth;
+  // Middleware to verify JWT
+  const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+  
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized: No token provided' });
+    }
+  
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+      }
+      req.userId = decoded.id; // Attach user ID to request object
+      next();
+    });
+  };
+  
+  module.exports = {withAuth,verifyToken};
   
